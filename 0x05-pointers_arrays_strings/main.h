a243de3bf993c3bc63707b47c701219e0578b61a
T0S1N8 <winnerokenwa10@gmail.com>
2023-05-03 02:38:35 -0700

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 
 #include "main.h"
 #include <stdio.h>
+
+_putchar
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,5 +15,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-_putchar
 #endif
